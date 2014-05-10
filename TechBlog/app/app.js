b76d23689c5a1479574ab1bca5a6c6575fb6ee76
MatthewHartz <matthew.vincent.hartz@gmail.com>
2014-05-10 02:06:12 -0700

    Added content to app.js and controller.js Created another view and populated form

--- TechBlog/app/app.js
+++ TechBlog/app/app.js
@@ -1,1 +1,11 @@
-﻿
+﻿var app = angular.module('techBlogApp', []);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/login',
+        {
+            controller: 'AccountController',
+            templateUrl: '/app/partials/login.cshtml'
+        })
+        .otherwise({ redirectTo: '/home'});
+});
--- TechBlog/app/controllers/controllers.js
+++ TechBlog/app/controllers/controllers.js
@@ -1,1 +1,15 @@
-﻿
+﻿app.controller('AccountController', function($scope) {
+    init();
+
+    function init() {
+        $scope.loggedIn = false;
+    }
+
+    function logIn() {
+        $scope.loggedIn = true;
+    }
+
+    function logOut() {
+        $scope.loggedIn = false;
+    }
+});
