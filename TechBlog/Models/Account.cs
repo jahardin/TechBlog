5390e576dcadd4abfef5915c95b2d7886ef71365
MatthewHartz <matthew.vincent.hartz@gmail.com>
2014-05-09 23:04:19 -0700

    First Commmit

---
+++ TechBlog/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechBlog.Models
+{
+    public class Account
+    {
+        int Id { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+    }
+}
---
+++ TechBlog/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechBlog.Models
+{
+    public class User
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime DateOfBirth { get; set; }
+        Uri ImageUrl { get; set; }
+        int AccountId { get; set; }
+    }
+}
